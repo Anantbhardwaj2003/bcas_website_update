26ff13387ad0561cc72568d9da37045b458c5200
Chiranjit <cmandal2003@gmail.com>
2024-01-29 21:38:16 +0530

    Add Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+
+function Header(props) {
+    return (
+        <div>
+            <div className="Group1" style={{ width: 1444, height: 272, position: 'relative' }}>
+                <img className="Image2" style={{ width: 161.45, height: 152, left: 1271.52, top: 65, position: 'absolute' }} src="https://via.placeholder.com/161x152" />
+                <div className="Rectangle1" style={{ width: 1444, height: 190, left: 0, top: 40, position: 'absolute', background: '#80B192', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }} />
+                <div className="Rectangle2" style={{ width: 1444, height: 42, left: 0, top: 230, position: 'absolute', background: '#88292F' }} />
+                <div className="Rectangle3" style={{ width: 1444, height: 42, left: 0, top: 0, position: 'absolute', background: '#88292F' }} />
+                <div className="BhaskaracharyaCollegeOfAppliedSciences" style={{ width: 1079, left: 64, top: 65, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 32, fontFamily: 'Outfit', fontWeight: '600', textTransform: 'uppercase', lineHeight: 42.40, wordWrap: 'break-word' }}>bhaskaracharya college of applied sciences</div>
+                <div className="About" style={{ width: 92, left: 106, top: 235, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 26, fontFamily: 'Outfit', fontWeight: '700', textTransform: 'uppercase', wordWrap: 'break-word' }}>about </div>
+                <div className="Apply" style={{ width: 82, left: 249, top: 235, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 26, fontFamily: 'Outfit', fontWeight: '700', textTransform: 'uppercase', wordWrap: 'break-word' }}>apply</div>
+                <div className="Faculty" style={{ width: 116, left: 887, top: 235, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 26, fontFamily: 'Outfit', fontWeight: '700', textTransform: 'uppercase', wordWrap: 'break-word' }}>faculty</div>
+                <div className="Academics" style={{ width: 154, left: 395, top: 235, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 26, fontFamily: 'Outfit', fontWeight: '700', textTransform: 'uppercase', wordWrap: 'break-word' }}>academics</div>
+                <div className="NewsEvents" style={{ width: 203, left: 620, top: 235, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 26, fontFamily: 'Outfit', fontWeight: '700', textTransform: 'uppercase', wordWrap: 'break-word' }}>news & events</div>
+                <img className="Image1" style={{ width: 169, height: 166, left: 35, top: 52, position: 'absolute' }} src="https://via.placeholder.com/169x166" />
+                <div className="Rectangle53" style={{ width: 165, height: 30, left: 5, top: 5, position: 'absolute', background: 'white', borderRadius: 21 }} />
+                <div className="Rectangle55" style={{ width: 213, height: 30, left: 1195, top: 236, position: 'absolute', background: 'white', borderRadius: 21 }} />
+                <img className="Image4" style={{ width: 140, height: 83, left: 1165, top: 86, position: 'absolute' }} src="https://via.placeholder.com/140x83" />
+                <div className="Login" style={{ left: 1348, top: 5, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 26, fontFamily: 'Outfit', fontWeight: '700', textTransform: 'uppercase', wordWrap: 'break-word' }}>login</div>
+                <div className="QuickLinks" style={{ left: 1114, top: 5, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 26, fontFamily: 'Outfit', fontWeight: '700', textTransform: 'uppercase', wordWrap: 'break-word' }}>quick links</div>
+                <div className="UniversityOfDelhiNaacGradeANirfRank22AmongstColleges" style={{ left: 409, top: 109, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontFamily: 'Outfit', fontWeight: '500', textTransform: 'uppercase', lineHeight: 26.50, wordWrap: 'break-word' }}>University Of Delhi<br />NAAC Grade A++ <br />NIRF Rank # 22 (Amongst Colleges)<br /></div>
+            </div>
+            <div className="Search" style={{ width: 116, textAlign: 'center', color: 'black', fontSize: 26, fontFamily: 'Outfit', fontWeight: '500', textTransform: 'capitalize', wordWrap: 'break-word' }}>search</div>
+            <div className="Slider" style={{ width: 1526.58, height: 609.26, position: 'relative' }}>
+                <img className="Rectangle2" style={{ width: 1526.58, height: 609.26, left: 0, top: 0, position: 'absolute' }} src="https://via.placeholder.com/1527x609" />
+                <div className="Rectangle18" style={{ width: 1524.49, height: 609, left: 0, top: 0, position: 'absolute', background: 'linear-gradient(180deg, rgba(0, 0, 0, 0.44) 0%, rgba(0, 0, 0, 0.41) 100%, rgba(0, 0, 0, 0) 100%)' }} />
+                <div className="SmartStudentsCleverTech" style={{ width: 1248, height: 194, left: 28, top: 98, position: 'absolute', color: 'white', fontSize: 66, fontFamily: 'Outfit', fontWeight: '600', textTransform: 'uppercase', lineHeight: 60.06, wordWrap: 'break-word' }}>SMART STUDENTS,<br />CLEVER TECH<br /></div>
+                <div className="WithTheHelpOfSmartphoneAndFitnessDataFromUwStudentsLikeMeiWuAnIschoolTeamIsStudyingAndPredictingHuskyHealthAndAcademicSuccess" style={{ width: 858.70, height: 82.85, left: 35, top: 292, position: 'absolute', color: 'white', fontSize: 33, fontFamily: 'Outfit', fontWeight: '400', textTransform: 'uppercase', wordWrap: 'break-word' }}>With the help of smartphone and fitness data from UW students<br />like Mei Wu, an iSchool team is studying and predicting Husky<br /> health and academic success.</div>
+                <div className="Line11" style={{ width: 666.01, height: 0, left: 28, top: 259, position: 'absolute', border: '12px #B8AC8C solid' }}></div>
+                <div className="Frame1" style={{ width: 94.47, height: 83.69, left: 1231.85, top: 343.97, position: 'absolute' }} />
+                <div className="Ellipse1" style={{ width: 27, height: 25, left: 642, top: 566, position: 'absolute', background: '#D9D9D9', borderRadius: 9999 }} />
+                <div className="Ellipse2" style={{ width: 27, height: 25, left: 680, top: 566, position: 'absolute', background: 'white', borderRadius: 9999 }} />
+                <div className="Ellipse3" style={{ width: 27, height: 25, left: 717, top: 566, position: 'absolute', background: '#D9D9D9', borderRadius: 9999 }} />
+                <div className="Ellipse4" style={{ width: 27, height: 25, left: 754, top: 566, position: 'absolute', background: '#D9D9D9', borderRadius: 9999 }} />
+                <div className="Ellipse5" style={{ width: 27, height: 25, left: 790, top: 566, position: 'absolute', background: '#D9D9D9', borderRadius: 9999 }} />
+                <div className="Ellipse6" style={{ width: 27, height: 25, left: 828, top: 566, position: 'absolute', background: '#D9D9D9', borderRadius: 9999 }} />
+            </div>
+            <img className="Login11" style={{ width: 26, height: 26 }} src="https://via.placeholder.com/26x26" />
+            <img className="Image12" style={{ width: 28, height: 26 }} src="https://via.placeholder.com/28x26" />
+            <img className="Image13" style={{ width: 27, height: 27 }} src="https://via.placeholder.com/27x27" />
+            <img className="Image14" style={{ width: 27, height: 27 }} src="https://via.placeholder.com/27x27" />
+            <img className="Image15" style={{ width: 29, height: 29 }} src="https://via.placeholder.com/29x29" />
+            <img className="Link1" style={{ width: 34, height: 34 }} src="https://via.placeholder.com/34x34" />
+            <div className="Rectangle52" style={{ width: 179, height: 36, background: '#D9D9D9' }}></div>
+            <img className="Image16" style={{ width: 22, height: 22 }} src="https://via.placeholder.com/22x22" />
+            <img className="Image17" style={{ width: 150, height: 84 }} src="https://via.placeholder.com/150x84" />
+            <img className="G20Logo1" style={{ width: 142, height: 142 }} src="https://via.placeholder.com/142x142" />
+            <div className="5087597" style={{ textAlign: 'center', color: 'white', fontSize: 20, fontFamily: 'Outfit', fontWeight: '500', textTransform: 'uppercase', lineHeight: 26.50, wordWrap: 'break-word' }}>011-508-7597</div>
+            <div className="BhaskaracharyaCollegeBcasDuAcIn" style={{ width: 535, height: 41, textAlign: 'center', color: 'white', fontSize: 20, fontFamily: 'Outfit', fontWeight: '500', textTransform: 'lowercase', lineHeight: 26.50, wordWrap: 'break-word' }}>bhaskaracharya.college@bcas.du.ac.in</div>
+            <img className="PhoneCall1" style={{ width: 19, height: 19 }} src="https://via.placeholder.com/19x19" />
+            <img className="Email1" style={{ width: 30, height: 30 }} src="https://via.placeholder.com/30x30" />
+        </div>
+    );
+}
+
+export default Header;
