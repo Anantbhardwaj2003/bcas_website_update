6f259dffa73a238717cce3bdb895f5cf179f8e35
Chiranjit <cmandal2003@gmail.com>
2024-01-30 01:55:42 +0530

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from './components/Header'
-// import Footer from './components/Footer'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <Header />
-      {/* <Footer /> */}
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,150 @@
+import React from "react";
+import "./Footer.css";
+
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="main-container">
+                <div>
+                    <div className="footer-title">links</div>
+                    <div className="footer-flex">
+                        <div className="footer-links-container">
+                            <ul className="footer-links-list">
+                                <li>
+                                    <a href="/">courses</a>
+                                </li>
+                                <li>
+                                    <a href="/">fees</a>
+                                </li>
+                                <li>
+                                    <a href="/">contact us</a>
+                                </li>
+                                <li>
+                                    <a href="/">updates</a>
+                                </li>
+                                <li>
+                                    <a href="/">student login</a>
+                                </li>
+                                <li>
+                                    <a href="/">faculty login</a>
+                                </li>
+                                <li>
+                                    <a href="/">work with us</a>
+                                </li>
+                                <li>
+                                    <a href="/">placement</a>
+                                </li>
+                                <li>
+                                    <a href="/">reults</a>
+                                </li>
+                                <li>
+                                    <a href="/">vocational course</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="footer-links-container">
+                            <ul className="footer-links-list">
+                                <li>
+                                    <a href="/">courses</a>
+                                </li>
+                                <li>
+                                    <a href="/">fees</a>
+                                </li>
+                                <li>
+                                    <a href="/">contact us</a>
+                                </li>
+                                <li>
+                                    <a href="/">updates</a>
+                                </li>
+                                <li>
+                                    <a href="/">student login</a>
+                                </li>
+                                <li>
+                                    <a href="/">faculty login</a>
+                                </li>
+                                <li>
+                                    <a href="/">work with us</a>
+                                </li>
+                                <li>
+                                    <a href="/">placement</a>
+                                </li>
+                                <li>
+                                    <a href="/">reults</a>
+                                </li>
+                                <li>
+                                    <a href="/">vocational course</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="footer-links-container">
+                            <ul className="footer-links-list">
+                                <li>
+                                    <a href="/">courses</a>
+                                </li>
+                                <li>
+                                    <a href="/">fees</a>
+                                </li>
+                                <li>
+                                    <a href="/">contact us</a>
+                                </li>
+                                <li>
+                                    <a href="/">updates</a>
+                                </li>
+                                <li>
+                                    <a href="/">student login</a>
+                                </li>
+                                <li>
+                                    <a href="/">faculty login</a>
+                                </li>
+                                <li>
+                                    <a href="/">work with us</a>
+                                </li>
+                                <li>
+                                    <a href="/">placement</a>
+                                </li>
+                                <li>
+                                    <a href="/">reults</a>
+                                </li>
+                                <li>
+                                    <a href="/">vocational course</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div>
+                        <a href="https://www.cutercounter.com/?id=hxpdqpa" target="_blank">
+                            <img src="https://www.cutercounter.com/stats.php?id=hxpdqpa&nd=6&style=7" border="0" alt="hit counter" />
+                        </a>
+                    </div>
+                </div>
+                <div className="divider" />
+                <div className="reach-us">
+                    <div style={{ paddingTop: "1rem" }}>
+                        <iframe
+                            width={480}
+                            height={240}
+                            loading="lazy"
+                            allowFullScreen=""
+                            referrerPolicy="no-referrer-when-downgrade"
+                            src="https://maps.google.com/maps?q=Bhaskaracharya+College+Of+Applied+Sciences&t=&z=15&ie=UTF8&iwloc=&output=embed"
+                        ></iframe>
+                    </div>
+                    <div>
+                        <p className="bhaskaracharya">BHASKARACHARYA COLLEGE OF APPLIED SCIENCES</p>
+                        <p className="university-of-delhi">
+                            UNIVERSITY OF DELHI
+                            <br />
+                            NAAC GRADE A++ <br />
+                            NIRF RANK # 22 (AMONGST COLLEGES)
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <p className="copyrights">
+                © 2023 BHASKARACHARYA COLLEGE OF APPLIED SCIENCES RIGHTS RESERVED.DEVELOPED BY TECH-TEAM BCAS
+            </p>
+        </footer>
+    );
+};
+export default Footer;
